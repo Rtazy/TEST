6b7fa7b79fe28eddb3ae531d0cb8c8b58e2d5001
Rtazi <149195454+Rtazy@users.noreply.github.com>
2023-12-06 13:09:02 +0100

    Update BE2.py

--- BE2.py
+++ BE2.py
@@ -1,6 +1,12 @@
 from flask import Flask
 
-app = Flask(__name__)
+
+app= Flask(__name__)
+app.config['DB_USER']='rimetazi_AlNour'
+app.config['DB_NAME']='rimetazi_AlNour'
+app.config['DB_HOST']='sql.bsite.net\MSSQL2016'
+app.config['DB_PASSWORD']='1202'
+
 
 @app.route('/profile')
 def profile():
