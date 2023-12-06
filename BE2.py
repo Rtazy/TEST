4d4bc0d3faa175bd59b84033238e39bacebcd6eb
Rtazi <149195454+Rtazy@users.noreply.github.com>
2023-12-06 13:24:41 +0100

    Update BE2.py

--- BE2.py
+++ BE2.py
@@ -10,7 +10,28 @@ app.config['DB_PASSWORD']='1202'
 def connect_db():
    return pg.connect(host=app.config['DB_HOST'],dbname=app.config['DB_NAME'],user=app.config['DB_USER'],password=app.config['DB_PASSWORD'])
 
+def insert_data_mobin(table_name, columns, column_values):
+   cursor = connect_db().cursor()
+    
+            # Create an INSERT query with dynamic columns
+   
+   query = sql.SQL("INSERT INTO {} ({}) VALUES ({})").format(
+                sql.Identifier(table_name),
+                sql.SQL(', ').join(map(sql.Identifier, columns)),
+                sql.SQL(', ').join(sql.Placeholder() * len(columns))
+            )
 
+            # Execute the query with values
+   cursor.execute(query, column_values)
+
+@app.route("/Ajouter_Annonce", methods=['GET', 'POST'])
+def add_announcement():
+    if request.method == 'POST':
+        Ann_Title = request.form['title']
+        Ann_Text = request.form['text']
+        insert_data_mobin('Announcement', ['title', 'text'], [Ann_Title, Ann_Text])
+
+    return render_template("Frontend/CreateAn.html")
 @app.route('/profile')
 def profile():
     return 'Hello, this is a test response from the /profile route!'
