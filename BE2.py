0c627d1a97120e153743432193bbc7d8d9e114e0
Rtazi <149195454+Rtazy@users.noreply.github.com>
2023-12-06 13:15:33 +0100

    Update BE2.py

--- BE2.py
+++ BE2.py
@@ -7,6 +7,9 @@ app.config['DB_NAME']='rimetazi_AlNour'
 app.config['DB_HOST']='sql.bsite.net\MSSQL2016'
 app.config['DB_PASSWORD']='1202'
 
+def connect_db():
+   return pg.connect(host=app.config['DB_HOST'],dbname=app.config['DB_NAME'],user=app.config['DB_USER'],password=app.config['DB_PASSWORD'])
+
 
 @app.route('/profile')
 def profile():
