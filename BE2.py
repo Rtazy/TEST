2fcbbc0cfa9043653e767cc328f0f88d9d46f92e
Rtazi <149195454+Rtazy@users.noreply.github.com>
2023-12-07 05:09:10 +0100

    Update BE2.py

--- BE2.py
+++ BE2.py
@@ -1,14 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import psycopg2 as pg
+from psycopg2 import sql
+from flask import jsonify
+import time
+from datetime import datetime
 
-
-app= Flask(__name__)
-app.config['DB_USER']='rimetazi_AlNour'
-app.config['DB_NAME']='rimetazi_AlNour'
-app.config['DB_HOST']='sql.bsite.net\MSSQL2016'
-app.config['DB_PASSWORD']='1202'
+app = Flask(__name__)
+app.config['DB_USER'] = 'rimetazi_AlNour'
+app.config['DB_NAME'] = 'rimetazi_AlNour'
+app.config['DB_HOST'] = 'sql.bsite.net'
+app.config['DB_PORT'] = 5432  # Specify the port here
+app.config['DB_PASSWORD'] = '1202'
 
 def connect_db():
-   return pg.connect(host=app.config['DB_HOST'],dbname=app.config['DB_NAME'],user=app.config['DB_USER'],password=app.config['DB_PASSWORD'])
+    return pg.connect(
+        host=app.config['DB_HOST'],
+        port=app.config['DB_PORT'],
+        dbname=app.config['DB_NAME'],
+        user=app.config['DB_USER'],
+        password=app.config['DB_PASSWORD']
+    )
 
 def insert_data_mobin(table_name, columns, column_values):
    cursor = connect_db().cursor()
