71a077b6c72b99b625fc8822f32350b2e45e8af2
Rtazi <149195454+Rtazy@users.noreply.github.com>
2023-12-09 09:58:11 +0100

    Update BE2.py

--- BE2.py
+++ BE2.py
@@ -15,6 +15,35 @@ conn_string = (
     f"postgresql://{app.config['DB_USER']}:{app.config['DB_PASSWORD']}@"
     f"{app.config['DB_HOST']}/{app.config['DB_NAME']}?sslmode=require"
 )
+
+
+#create the code in HTML for ajax:
+
+def to_htmltable(lst):
+   
+   for item in lst:
+     res+='<td>'
+
+     for tem in item:
+         res+="<tb>{}</tb>".format(tem)
+     res+='</td>'
+              
+   return res
+
+def display(querry):
+
+   res=connect_db().cursor().execute(querry).fetchall()
+   return to_htmltable(res)
+
+#send the code to AJAX?? 
+
+@app.route('/api/get_data')
+def get_data(querry):
+    # Your logic to generate or fetch updated data
+    display(querry)
+    updated_data = f"Updated data at {time.strftime('%H:%M:%S')}"
+    return jsonify({querry: updated_data})
+
 def connect_db():
  return pg.connect(conn_string)
 
@@ -32,6 +61,150 @@ def insert_data_mobin(table_name, columns, column_values):
     except pg.Error as e:
         # Handle the exception
         print(f"Error: {e}")
+    
+def insert_data(table_name, maxvarbin_column, other_columns, binary_data, other_column_values):
+    #####
+            cursor = connect_db().cursor()
+    
+            # Create an INSERT query with dynamic columns
+            columns = [maxvarbin_column] + other_columns
+            query = sql.SQL("INSERT INTO {} ({}) VALUES ({})").format(
+                sql.Identifier(table_name),
+                sql.SQL(', ').join(map(sql.Identifier, columns)),
+                sql.SQL(', ').join(sql.Placeholder() * len(columns))
+            )
+
+            # Execute the query with values
+            values = [pg.Binary(binary_data)] + other_column_values
+            cursor.execute(query, values)
+
+
+    # Commit the transaction to persist changes
+            connect_db().commit()
+
+    # Close the cursor and connection when done
+            cursor.close()
+            connect_db().close()
+########################################
+########################################
+def Select_entity(table,pkcol,entity_id):
+            cursor = connect_db().cursor()
+          
+
+            query = f"SELECT * FROM {table} WHERE {pkcol} = {entity_id}"
+
+            # Execute the query with the entity_id parameter
+            cursor.execute(query)
+
+            # Fetch the result
+            entity = cursor.fetchone()
+
+            return entity
+########################################
+########################################
+def Select_entity(table,pkcol,entity_id):
+            cursor = connect_db().cursor()
+          
+
+            query = f"SELECT * FROM {table} WHERE {pkcol} = {entity_id}"
+
+            # Execute the query with the entity_id parameter
+            cursor.execute(query)
+
+            # Fetch the result
+            entity = cursor.fetchone()
+
+            return entity
+########################################
+########################################
+
+def Del_data(table_name, primary_key_column, primary_key_value):
+   
+    cursor = connect_db().cursor()
+
+    # Create an INSERT query with dynamic columns
+    
+    query = sql.SQL("DELETE FROM {} WHERE {}= %s").format(
+        sql.Identifier(table_name), sql.Identifier(primary_key_column))
+
+
+   # Execute the query with the primary key value
+    cursor.execute(query, (primary_key_value,))
+
+    # Commit the transaction to persist changes
+    connect_db().commit()
+
+    # Close the cursor and connection when done
+    cursor.close()
+    connect_db().close()
+########################################
+########################################
+
+def Sel_data_one(table_name, primary_key_column, primary_key_value):
+   
+    cursor = connect_db().cursor()
+
+    # Create an INSERT query with dynamic columns
+    
+    query = sql.SQL("SELECT * FROM {} WHERE {}= %s").format(
+        sql.Identifier(table_name), sql.Identifier(primary_key_column))
+ 
+
+   # Execute the query with the primary key value
+    cursor.execute(query, (primary_key_value,))
+    err=0
+
+    ret= cursor.fetchone()
+    if ret != None:
+       err=1
+    cursor.close()
+    connect_db().close()
+    return err
+########################################
+def Sel_data_all(table_name, primary_key_column, primary_key_value):
+   
+    cursor = connect_db().cursor()
+
+    # Create an INSERT query with dynamic columns
+    
+    query = sql.SQL("SELECT * FROM {} WHERE {}= %s").format(
+        sql.Identifier(table_name), sql.Identifier(primary_key_column))
+
+
+   # Execute the query with the primary key value
+    cursor.execute(query, (primary_key_value,))
+
+    ret= cursor.fetchall()
+    cursor.close()
+    connect_db().close()
+    return ret
+########################################
+@app.route('/nouveau_beneficiere',methods=['POST','GET'])
+def add_ben():
+    if request.form['methods']==['POST']:
+  
+     Name=request.form['Name']
+     gender=request.form['gender']
+     Disability_Category=request.form['Disability_Category']
+     documents=request.files['documents']
+     joining_date=request.form['joining_date']
+     b_date=request.form['Birthdate']
+     b_place=request.form['Birth_Place']
+     Disability_sd=request.form['Disability_startdate']
+     Disability_sd_con=datetime.strptime(Disability_sd,'%Y-%m-%d').date()
+     bdate_conv=datetime.strptime(b_date,'%Y-%m-%d').date()
+     jdate_conv=datetime.strptime(joining_date,'%Y-%m-%d').date()
+     docs_bin= documents.read()
+     Cols=['Full_Name','Birthdate','Birth_Place','join_date','Disability_Start_Date','Gender','Disability_Category' ]
+     u=Select_entity('Beneficiary',['Full_Name','Birthdate','Birth_Place','Gender','Disability_Category' ],[Name,bdate_conv, b_place, gender,Disability_Category] )
+     hey=""
+     if u != None:
+        hey="Ce membre exist deja."
+     else:
+         insert_data("Beneficiaries", " Documents" ,Cols, docs_bin,[Name, bdate_conv, b_place,jdate_conv,Disability_sd_con,gender,Disability_Category] )
+         hey="Ce membre a ete ajoute"
+   
+    return render_template('Frontend/ADDBenf.html')
 
 @app.route("/Ajouter_Annonce", methods=['GET', 'POST'])
 def add_announcement():
